1b399d655d99acb5d75ca300185d1a2ab372e5af
zeeshan <fareedzeeshan868@gmail.com>
2025-05-01 15:54:36 +0500

    new changes

--- dom/script.js
+++ dom/script.js
@@ -1,4 +1,10 @@
 console.log("hello")
-console.log(window)
 window.console.log("hello pakistan")
+console.log(window)
+console.log(window.document)
+console.dir(window.document)
+console.dir(window.document.body)
+console.dir(document.body.childNodes);
 console.dir(document.body.childNodes[1]);
+console.dir(document.body.style.background="green");
+document.body.childNodes[1].innertext="this is chnage"
