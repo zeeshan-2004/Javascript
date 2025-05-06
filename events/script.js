ee8286f1c9b30a7b4c1ab50009d58efacda5bb12
zeeshan <fareedzeeshan868@gmail.com>
2025-05-06 22:14:46 +0500

    update

--- events/script.js
+++ events/script.js
@@ -70,4 +70,84 @@ togl.addEventListener("click",()=>{
 // modeBtn.addEventListener("click", change);
 
 
+// common mouse events
+
+let ev=document.querySelector(".evnt");
+ev.addEventListener("mousedown",()=>{
+    console.log("mousedown");
+})
+ev.addEventListener("mouseup",()=>{
+    console.log("mouseup");
+})
+ev.addEventListener("mousemove",()=>{
+    console.log("mousemove");
+})
+ev.addEventListener("mouseenter",()=>{
+    console.log("mouseenter");
+})
+ev.addEventListener("mouseleave",()=>{
+    console.log("mouseleave");
+})
+ev.addEventListener("mouseover",()=>{
+    console.log("mouseover");
+})
+ev.addEventListener("mouseout",()=>{
+    console.log("mouseout");
+})
+ev.addEventListener("contextmenu",()=>{
+    console.log("contextmenu");
+})
+
+
+// keyborad events
+let key1=document.querySelector(".keyb");
+key1.addEventListener("keypress",()=>{
+     console.log("kyepress");
+})
+let key2=document.querySelector(".tab");
+key2.addEventListener("keydown",(e)=>{
+    console.log("tab key down",e.key);
+}) 
+key2.addEventListener("keyup",(e)=>{
+    console.log("release key",e.key);
+}) 
+
+// form events
+let form=document.querySelector("form");
+// form.addEventListener("submit",()=>{
+//     event.preventDefault();
+//     console.log("you submit form")
+// })
+// form.addEventListener("change",()=>{
+//     event.preventDefault();
+//     console.log("you submit form")
+// })
+// form.addEventListener("input",()=>{
+//     event.preventDefault();
+//     console.log("changed value",event.target.value)
+// })
+
+form.addEventListener("reset",()=>{
+    event.preventDefault();
+    console.log("reset",)
+})
+form.addEventListener("invalid",()=>{
+    event.preventDefault();
+    console.log("invalid",event.target)
+},true);
+
+
+let form1=document.querySelector(".ch");
+// form1.addEventListener("change",()=>{
+//     console.log("chnage value",event.target.value)
+// });
+// form1.addEventListener("focus",()=>{
+//     console.log("focus",)
+// });
+// form1.addEventListener("blur",()=>{
+//     console.log("blur")
+// });
+form1.addEventListener("reset",()=>{
+    console.log("reset")
+});
 
